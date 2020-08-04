90dbf610514638de551e6b31433a580c8796088c
asapkent <jeffersalex94@gmail.com>
2020-08-04 18:48:03 -0400

    Set up record and stop recording buttons. Also added constraints.

--- PtichPerfect/ViewController.swift
+++ PtichPerfect/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordButtonPressed(_ sender: Any) {
+        print("Record has been pressed!")
+        recordingLabel.text = "Recording in Progress"
+    }
+    
+    @IBAction func stopRecordingButtonPressed(_ sender: Any) {
+        print("Stop Recording has been pressed!")
+        recordingLabel.text = "Tap to Record"
+    }
 }
 
